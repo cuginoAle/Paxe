26e39fa03e670b03f0fc64c8c8809a165b00180b
Alessio Carnevale <alessio.carnevale@gmail.com>
2019-07-23 16:55:19 +0200

    creating logger component to support multi-stack messages

---
+++ scripts/logger.js
@@ -0,0 +1,37 @@
+const readline = require('readline')
+const chalk = require('chalk')
+
+const logger = () => {
+  let stack = []
+  const print = () => {
+    readline.cursorTo(process.stdout, 0, 8)
+    readline.clearScreenDown(process.stdout)
+    const rows = Math.floor((process.stdout.rows - 9) / 2)
+    process.stdout.write(`${stack.map(ctx => ctx.slice(-rows).join('\n')).join(`\n${chalk.green('---------------------------------')}\n`)}\n`)
+  }
+
+  return {
+    newLine: (msg, stackIndex = 0) => {
+      if (stack[stackIndex]) {
+        stack[stackIndex].push(msg)
+      } else {
+        stack[stackIndex] = [msg]
+      }
+      print()
+    },
+    sameLine: (msg, stackIndex = 0) => {
+      if (stack[stackIndex]) {
+        stack[stackIndex].pop()
+        stack[stackIndex].push(msg)
+      } else {
+        stack[stackIndex] = [msg]
+      }
+      print()
+    },
+    clear: () => {
+      console.clear()
+      stack = {}
+    }
+  }
+}
+module.exports = logger()
