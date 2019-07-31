9db7731a854d6aa6a01c87bcf9a37115fc367bbf
Alessio Carnevale <alessio.carnevale@gmail.com>
2019-07-31 12:19:27 +0200

    adding defaults to logger

--- scripts/logger.js
+++ scripts/logger.js
@@ -1,12 +1,12 @@
 const readline = require('readline')
 const chalk = require('chalk')
 
-const logger = () => {
+const logger = (stackCount = 2, offsetTop = 8) => {
   let stack = []
   const print = () => {
-    readline.cursorTo(process.stdout, 0, 8)
+    readline.cursorTo(process.stdout, 0, offsetTop)
     readline.clearScreenDown(process.stdout)
-    const rows = Math.floor((process.stdout.rows - 9) / 2)
+    const rows = Math.floor((process.stdout.rows - offsetTop - 1) / stackCount)
     process.stdout.write(`${stack.map(ctx => ctx.slice(-rows).join('\n')).join(`\n${chalk.green('---------------------------------')}\n`)}\n`)
   }
 
